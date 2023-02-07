9b9283b3231174e1d5b5ebe21f0f85f7a3c0774a
Bryan Lincoln <bryanufg@gmail.com>
2023-02-07 15:23:44 -0300

    fix: replace gdown with github releases

--- soccer_twos/package.py
+++ soccer_twos/package.py
@@ -5,21 +5,20 @@ import platform
 import shutil
 import stat
 import zipfile
-
-import gdown
+import requests, zipfile, io
 
 if platform.system() == "Linux":
     TRAINING_ENV_PATH = "linux-x86_64/soccer-twos/soccer-twos.x86_64"
     ROLLOUT_ENV_PATH = "linux-x86_64/watch-soccer-twos/watch-soccer-twos.x86_64"
-    G_ID = "150nGbjgkAeIr8YfcK-blbZ9P7GYE7bms"  # linux.x86_64
+    ZIP_URL = "https://github.com/bryanoliveira/soccer-twos-env/releases/download/0.1.13/linux-x86_64.zip"  # linux.x86_64
 elif platform.system() == "Windows":
     TRAINING_ENV_PATH = "windows-x86_64/soccer-twos/UnityEnvironment.exe"
     ROLLOUT_ENV_PATH = "windows-x86_64/watch-soccer-twos/UnityEnvironment.exe"
-    G_ID = "1fGCMb47Pbvsc2c8veI3CmMh4ieXsuOq1"  # windows.x86_64
+    ZIP_URL = "https://github.com/bryanoliveira/soccer-twos-env/releases/download/0.1.13/windows-x86_64.zip"  # windows.x86_64
 elif platform.system() == "Darwin":
     TRAINING_ENV_PATH = "mac_os/soccer-twos.app/Contents/MacOS/UnityEnvironment"
     ROLLOUT_ENV_PATH = "mac_os/watch-soccer-twos.app/Contents/MacOS/UnityEnvironment"
-    G_ID = "1fuEkPyr0z0UM5hpe9fLSK8v243CuqJd6"  # mac_os
+    ZIP_URL = "https://github.com/bryanoliveira/soccer-twos-env/releases/download/0.1.13/mac_os.zip"  # mac_os
 else:
     raise Exception("Unsupported OS")
 
@@ -43,16 +42,11 @@ def check_package():
 
         os.makedirs(__BIN_DIR, exist_ok=True)
         os.makedirs(os.path.join(__CURR_DIR, "temp"), exist_ok=True)
-        gdown.download(
-            "https://drive.google.com/uc?id=" + G_ID,
-            os.path.join(__CURR_DIR, "temp/soccer_twos.zip"),
-        )
 
+        req = requests.get(ZIP_URL)
+        zip_ref = zipfile.ZipFile(io.BytesIO(req.content))
         logging.debug("Unzipping...")
-        with zipfile.ZipFile(
-            os.path.join(__CURR_DIR, "temp/soccer_twos.zip"), "r"
-        ) as zip_ref:
-            zip_ref.extractall(__BIN_DIR)
+        zip_ref.extractall(__BIN_DIR)            
 
         try:
             # try adding execute permissions to the binary
@@ -72,6 +66,9 @@ def check_package():
         shutil.rmtree(os.path.join(__CURR_DIR, "./temp"))
 
         logging.debug("Package verification done.")
+        logging.info(
+            f"Soccer environment installed for {platform.system().upper()}."
+        )
     else:
         logging.debug(
             f"Binary envs found in '{TRAINING_ENV_PATH}' and '{ROLLOUT_ENV_PATH}'"
